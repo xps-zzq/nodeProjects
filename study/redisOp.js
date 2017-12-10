4658fe61944593a75f0826f313b86d3dfa195c0b
zhangzhiqin <49626809@qq.com>
2017-12-10 23:05:00 +0800

    ad

--- study/redisOp.js
+++ study/redisOp.js
@@ -1,3 +1,13 @@
 const electron = require('electron')
 const {BrowserWindow} = electron
 const {app} = electron
+const redis = require("redis");
+const client =     redis.createClient(6379, "127.0.0.1");
+
+
+document.getElementById("submitCmd").onclick = function (e) {
+    var inputCmd = document.getElementById("inputCmd").value;
+}
+function connectionLocal(ip, port) {
+
+}
