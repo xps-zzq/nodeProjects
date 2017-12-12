71cba30d010af837feac06c2956ef724e2e647e5
zhangzhiqin <49626809@qq.com>
2017-12-12 23:48:05 +0800

    ad

--- study/redisOp.js
+++ study/redisOp.js
@@ -2,12 +2,21 @@ const electron = require('electron')
 const {BrowserWindow} = electron
 const {app} = electron
 const redis = require("redis");
-const client =     redis.createClient(6379, "127.0.0.1");
+const client = redis.createClient(6379, "127.0.0.1");
 
 
 document.getElementById("submitCmd").onclick = function (e) {
-    var inputCmd = document.getElementById("inputCmd").value;
+    var val = document.getElementById("inputCmd").value;
+    var strings = val.toString().split(",");
+    set(strings[0], strings[1]);
+    console.log("set key value" + strings[0] + ":"+strings[1]);
+    client.get(strings[0]);
+};
+
+function set(key,value) {
+    client.set(key, value);
 }
+
 function connectionLocal(ip, port) {
 
 }
